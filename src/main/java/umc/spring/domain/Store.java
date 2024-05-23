e795dcd87d5a84aabc6edd3914f41e188c467668
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2024-05-24 05:11:48 +0900

    Feat: 특정 지역에 가게 추가 API 구현

--- src/main/java/umc/spring/domain/Store.java
+++ src/main/java/umc/spring/domain/Store.java
@@ -2,13 +2,22 @@ package umc.spring.domain;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
 import umc.spring.domain.common.BaseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
+@Setter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
+@DynamicUpdate
+@DynamicInsert
 public class Store extends BaseEntity {
 
     @Id
@@ -21,5 +30,19 @@ public class Store extends BaseEntity {
     @Column(nullable = false, length = 40)
     private String address;
 
+    @ColumnDefault("'0.0'")
     private Float score;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "region_id")
+    private Region region;
+
+    @OneToMany(mappedBy = "store", cascade = CascadeType.ALL)
+    private List<Review> reviewList = new ArrayList<>();
+    
+    // 가게 만들때 지역 넣기
+    public void setRegion(Region region) {
+        this.region = region;
+        region.getStoreList().add(this);
+    }
 }
