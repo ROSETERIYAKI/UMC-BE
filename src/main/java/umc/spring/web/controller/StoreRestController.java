4b0515dfffd58ae8d00cfba6818c31f8e0476ca5
hsuush <dlhyes703@naver.com>
2024-05-30 18:30:18 +0900

    shore : swagger 세팅

--- src/main/java/umc/spring/web/controller/StoreRestController.java
+++ src/main/java/umc/spring/web/controller/StoreRestController.java
@@ -1,5 +1,9 @@
 package umc.spring.web.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
@@ -31,6 +35,14 @@ public class StoreRestController {
     }
 
     @GetMapping("/{storeId}/missions")
+    @Operation(summary = "가게의 미션 목록 가져오기 API",description = "StoreMissionListDTO를 반환")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200",description = "OK, 성공"),
+    })
+    @Parameters({
+            @Parameter(name = "page", description = "페이지 수"),
+            @Parameter(name = "size", description = "페이지 사이즈"),
+    })
     public ApiResponse<StoreResponseDTO.StoreMissionListDTO> getStoreMissionList(@PathVariable(name = "storeId")Long storeId, @RequestParam(defaultValue = "1")Integer page, @RequestParam(defaultValue = "10")Integer size){
         Store store = storeCommandService.findById(storeId);
 
